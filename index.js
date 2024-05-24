4028d3cce3c759cb5deb9db3767459b4db75f948
Özer SUBAŞI <ozer.subasi@gmail.com>
2024-05-24 22:39:59 +0300

    mongodb added with mongoose

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const connectDB = require("./src/config/database");
 
 const app = express();
+
+connectDB();
+
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
