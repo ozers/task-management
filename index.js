fd1af4f8205c9f63fa6abf09f0f160787b6abd8c
Özer SUBAŞI <ozer.subasi@gmail.com>
2024-05-24 05:54:12 +0300

    dockerized the express app

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', async (req, res) => {
+    res.json('hello world')
 });
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
